c6dd3c4e79324df382b4833a7197a332a09a69e4
Van Paul Dayag <vcdayag@up.edu.ph>
2022-11-26 02:52:04 +0800

    feat: add more tests

--- testing.py
+++ testing.py
@@ -38,6 +38,90 @@ doTest("Invalid Typo Blank Program",
        """,
        shouldFail=True)
 
+print("\n USER OUTPUT \n")
+
+doTest("Print Yarn",
+       """
+        HAI
+        VISIBLE "YARN"
+        KTHXBYE
+       """)
+
+doTest("Print Numbr",
+       """
+        HAI
+        VISIBLE 1
+        KTHXBYE
+       """)
+
+doTest("Print Numbar",
+       """
+        HAI
+        VISIBLE 1.0
+        KTHXBYE
+       """)
+
+doTest("Print Variable",
+       """
+        HAI
+        VISIBLE VARIABLE
+        KTHXBYE
+       """)
+
+doTest("Print Expresion",
+       """
+        HAI
+        VISIBLE SUM OF 2 AN 4
+        KTHXBYE
+       """)
+
+print("\n VARIABLES \n")
+
+doTest("I HAS A",
+       """
+        HAI
+        I HAS A thing
+        KTHXBYE
+       """)
+
+doTest("ITZ literal Yarn",
+       """
+        HAI
+        I HAS A thing2 ITZ "some"
+        KTHXBYE
+       """)
+
+doTest("ITZ literal Numbr",
+       """
+        HAI
+        I HAS A thing2 ITZ 2
+        KTHXBYE
+       """)
+
+doTest("ITZ literal Numbar",
+       """
+        HAI
+        I HAS A thing2 ITZ 2.0
+        KTHXBYE
+       """)
+
+doTest("ITZ literal Troof",
+       """
+        HAI
+        I HAS A thing2 ITZ WIN
+        I HAS A thing3 ITZ FAIL
+        KTHXBYE
+       """)
+
+doTest("ITZ Expression",
+       """
+        HAI
+        I HAS A thing2 ITZ SUM OF 5 AN 4
+        KTHXBYE
+       """)
+
+print("\n OPERATIONS \n")
+
 doTest("Assignment Numbr",
        """
         HAI
@@ -59,6 +143,14 @@ doTest("Assignment Yarn",
         HAI
         VARIABLE R "variable"
         I HAS A VAR ITZ "this is a yarn"
+        KTHXBYE
+       """)
+
+doTest("Assignment Troof",
+       """
+        HAI
+        VARIABLE R WIN
+        I HAS A VAR ITZ FAIL
         KTHXBYE
        """)
 
