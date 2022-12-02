797fa4b8c93a21cd55acb60b03e6e422801203d7
Van Paul Dayag <vcdayag@up.edu.ph>
2022-12-02 10:20:59 +0800

    refactor: separate error to own file

--- parser.py
+++ parser.py
@@ -1,19 +1,8 @@
 from token_types import *
 from nodes import *
+from error import *
 
 ST = []
-
-class Error(Exception):
-    def __init__(self, token, cause) -> None:
-        self.token = token
-        self.cause = cause
-
-    def __repr__(self) -> str:
-        return f'Error at {self.token}: {self.cause}'
-
-    def __str__(self) -> str:
-        return f'Error at {self.token}: {self.cause}'
-
 
 class Parser:
     def __init__(self, tokens) -> None:
